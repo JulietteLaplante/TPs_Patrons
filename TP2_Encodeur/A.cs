a51d3eaa5f5420094a7d1bc95e78d66094c749a2
Yoann HAFFNER <yoyofrom68@gmail.com>
2020-02-28 10:37:24 -0500

    TP2 encodeur

---
+++ TP2_Encodeur/A.cs
@@ -0,0 +1,10 @@
+﻿namespace EncodeurJSON
+{
+    class A
+    {
+        public string aWord = "tabulation";
+        protected int anAnswer = 23;
+        private B anotherObject = new B("contenu");
+        private float[] anArray = new float[] { .25f, 2f, 45f, -.03f };
+    }
+}
---
+++ TP2_Encodeur/B.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace EncodeurJSON
+{
+    class B
+    {
+        private string someWord;
+
+        public B(string word)
+        {
+            someWord = word;
+        }
+    }
+}
