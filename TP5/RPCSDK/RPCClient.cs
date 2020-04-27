fe9d0900c8a5f95875f80a19714ddd1e8a65efe7
Liehko <juliette.laplante.jl@gmail.com>
2020-04-27 15:16:32 -0400

    Library rpc

---
+++ TP5/RPCSDK/RPCClient.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RPCSDK
+{
+    public class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties props;
+
+        public RpcClient()
+        {
+            //ouverture connection
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+
+            //paramètres
+            props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body.ToArray());
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    respQueue.Add(response);
+                }
+            };
+        }
+
+        public string Call(string message, string routKey)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: routKey,
+                basicProperties: props,
+                body: messageBytes);
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true);
+
+            return respQueue.Take();
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ TP5/UserSDK/User.cs
@@ -0,0 +1,48 @@
+﻿using RPCSDK;
+using System;
+
+
+namespace UserManager
+{
+    class User
+    {
+        public string name { get; set; }
+        public string prenom { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string[]user)
+        {
+            this.name = user[0];
+            this.prenom = user[1];
+            this.email = user[2];
+            this.username = user[3];
+        }
+
+        public static User GetUser(string username)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting user " + username);
+            var response = rpcClient.Call(username);
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+
+            //si l'username n'hexiste pas
+            if (response == "null")
+                return null;
+
+            return new User(response.Split(":"));
+
+        }
+    }
+
+
+    
+
+}
